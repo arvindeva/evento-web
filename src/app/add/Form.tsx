935bbc67fc8be1c0434a2f772c6132da9548148e
Arvindeva Wibisono <arvindeva@gmail.com>
2024-01-31 13:35:35 +0100

    search success

--- src/app/add/Form.tsx
+++ src/app/add/Form.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import { createClient } from "@/lib/supabase/client";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useQuery } from "@tanstack/react-query";
+import { useDebounce } from "@uidotdev/usehooks";
+import { useForm } from "react-hook-form";
 
 interface ProfileProps {
   profile: {
@@ -21,6 +23,11 @@ export default function Form(props: ProfileProps) {
   const supabase = createClient();
   const router = useRouter();
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [isSearching, setIsSearching] = React.useState(false);
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
+  const { register } = useForm();
 
   const getProfile = async () => {
     console.log("getProfile trigerred");
@@ -36,10 +43,48 @@ export default function Form(props: ProfileProps) {
     queryFn: getProfile,
   });
 
-  console.log(data?.data);
-
   if (error) {
     console.log(error.message);
   }
-  return <div className="form-widget">a</div>;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const fetchResults = async (searchTerm: string) => {
+    return await supabase
+      .from("events")
+      .select()
+      .textSearch("name", `${searchTerm}:*`);
+  };
+
+  React.useEffect(() => {
+    const searchHN = async () => {
+      let results = [];
+      setIsSearching(true);
+      if (debouncedSearchTerm) {
+        const data = await fetchResults(debouncedSearchTerm);
+        console.log(data);
+      }
+
+      setIsSearching(false);
+      // setResults(results);
+    };
+
+    searchHN();
+  }, [debouncedSearchTerm]);
+
+  return (
+    <div className="form-widget">
+      <div>
+        <label htmlFor="search_term">search</label>
+        <input
+          name="search_term"
+          placeholder="search event"
+          onChange={handleChange}
+        />
+      </div>
+      {results && results}
+    </div>
+  );
 }
