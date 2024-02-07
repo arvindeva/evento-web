55063c1dea8c21ba35f3bf06e8a37781c955c18f
arvindeva <arvindeva@gmail.com>
2024-02-07 09:25:29 +0100

    more whoopsie

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ export default withSentryConfig(nextConfig, {
 // Suppresses source map uploading logs during build
 silent: true,
 org: "evento-mvp",
-project: "javascript-react",
+project: "evento-mvp",
 }, {
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
