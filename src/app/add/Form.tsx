683723a5130efa357eb9c80b56c22203b717f782
Arvindeva Wibisono <arvindeva@gmail.com>
2024-02-07 19:26:42 +0100

    test select behavior

--- src/app/add/Form.tsx
+++ src/app/add/Form.tsx
@@ -12,9 +12,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import InfiniteScroll from 'react-infinite-scroll-component';
-
-
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface EventsResponse {
   itemsPerPage: number;
@@ -45,7 +43,7 @@ interface Setlist {
   };
 }
 
-// interface NotFoundResponse { 
+// interface NotFoundResponse {
 //   code: string;
 //   status: string;
 //   message: string;
@@ -55,7 +53,7 @@ interface Setlist {
 export default function Form() {
   const [selectedMbid, setSelectedMbid] = useState<string>("");
   const [eventResults, setEventResults] = useState<EventsResponse | null>(null);
-  const [eventList, setEventList] = useState<Setlist[]>([])
+  const [eventList, setEventList] = useState<Setlist[]>([]);
   const [year, setYear] = React.useState("");
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [hasMore, setHasMore] = useState<boolean>(false);
@@ -73,11 +71,11 @@ export default function Form() {
     setEventList(data.setlist);
 
     // check if theres more data to fetch
-    const difference = data.total - data.page * data.itemsPerPage
-    console.log(data)
-    console.log(difference)
+    const difference = data.total - data.page * data.itemsPerPage;
+    console.log(data);
+    console.log(difference);
     if (difference <= 0) {
-      setHasMore(false)
+      setHasMore(false);
     } else {
       setHasMore(true);
     }
@@ -94,9 +92,9 @@ export default function Form() {
         .json<EventsResponse>();
       setEventResults(data);
       setEventList(data.setlist);
-      const difference = data.total - data.page * data.itemsPerPage
+      const difference = data.total - data.page * data.itemsPerPage;
       if (difference <= 0) {
-        setHasMore(false)
+        setHasMore(false);
       } else {
         setHasMore(true);
       }
@@ -110,11 +108,11 @@ export default function Form() {
       console.log(data);
       setEventResults(data);
       setEventList(data.setlist);
-      const difference = data.total - data.page * data.itemsPerPage
+      const difference = data.total - data.page * data.itemsPerPage;
 
-      console.log(difference)
+      console.log(difference);
       if (difference <= 0) {
-        setHasMore(false)
+        setHasMore(false);
       } else {
         setHasMore(true);
       }
@@ -123,33 +121,37 @@ export default function Form() {
   }
 
   async function fetchMoreEvents() {
-
-    const url = year === ""
-      ? `${process.env.NEXT_PUBLIC_EVENTO_API_URL}/search/events/${selectedMbid}?p=${currentPage + 1}`
-      : `${process.env.NEXT_PUBLIC_EVENTO_API_URL}/search/events?artistMbid=${selectedMbid}&year=${year}&p=${currentPage + 1}`;
+    const url =
+      year === ""
+        ? `${
+            process.env.NEXT_PUBLIC_EVENTO_API_URL
+          }/search/events/${selectedMbid}?p=${currentPage + 1}`
+        : `${
+            process.env.NEXT_PUBLIC_EVENTO_API_URL
+          }/search/events?artistMbid=${selectedMbid}&year=${year}&p=${
+            currentPage + 1
+          }`;
 
     try {
       const data = await ky.get(url).json<EventsResponse>();
       console.log(data);
       setEventResults(data);
       setEventList([...eventList, ...data.setlist]);
-      const difference = data.total - data.page * data.itemsPerPage
-      console.log(difference)
+      const difference = data.total - data.page * data.itemsPerPage;
+      console.log(difference);
       if (difference <= 0) {
-        setHasMore(false)
+        setHasMore(false);
       } else {
         setHasMore(true);
       }
-      setCurrentPage(prevState => prevState + 1)
-
+      setCurrentPage((prevState) => prevState + 1);
     } catch (error) {
       console.log(error);
     }
   }
 
-
   return (
-    <div >
+    <div>
       <div className="form-widget p-4 flex flex-col gap-y-4">
         <Combobox getEventsByMbid={getEventsByMbid} />
         <Select value={year} onValueChange={handleValueChange}>
@@ -162,12 +164,13 @@ export default function Form() {
             </Button>
           </div>
           <SelectContent
-            ref={(ref) => {
-              if (!ref) return;
-              ref.ontouchstart = (e) => {
-                e.preventDefault();
-              }
-            }}>
+          // ref={(ref) => {
+          //   if (!ref) return;
+          //   ref.ontouchstart = (e) => {
+          //     e.preventDefault();
+          //   }
+          // }}
+          >
             {Array.from({ length: 30 }, (_, i) => (
               <SelectItem key={i} value={`${new Date().getFullYear() - i}`}>
                 {new Date().getFullYear() - i}
@@ -178,88 +181,100 @@ export default function Form() {
 
         {eventResults?.total! > 0 ? (
           <div className="text-slate-500 text-base dark:text-slate-300 mb-2 mt-4">
-            <span className="font-semibold">{eventResults!.total!}</span> {" "}results found
+            <span className="font-semibold">{eventResults!.total!}</span>{" "}
+            results found
           </div>
         ) : (
           <div className="text-slate-500 text-base dark:text-slate-300 mb-2 mt-4">
             No Results Found
           </div>
         )}
-
       </div>
       <div className="px-4 py-0">
+        {eventList?.length > 0 && (
+          <InfiniteScroll
+            dataLength={eventList.length} //This is important field to render the next data
+            next={fetchMoreEvents}
+            hasMore={hasMore}
+            loader={
+              <p className="text-center mt-10 mb-10">
+                <b>Loading...</b>
+              </p>
+            }
+            endMessage={
+              <p className="text-center mt-10 mb-10">
+                <b>You&apos;ve reached the end... :(</b>
+              </p>
+            }
+          >
+            <div className="flex flex-col gap-y-4">
+              {eventList.map((evento: Setlist) => {
+                const datearray = evento.eventDate.split("-");
+                var formattedDate =
+                  datearray[1] + "/" + datearray[0] + "/" + datearray[2];
+                const cardDate = new Date(formattedDate);
+                const monthNames = [
+                  "January",
+                  "February",
+                  "March",
+                  "April",
+                  "May",
+                  "June",
+                  "July",
+                  "August",
+                  "September",
+                  "October",
+                  "November",
+                  "December",
+                ];
 
-        {eventList?.length > 0 && <InfiniteScroll
-          dataLength={eventList.length} //This is important field to render the next data
-          next={fetchMoreEvents}
-          hasMore={hasMore}
-          loader={<p className="text-center mt-10 mb-10">
-            <b>Loading...</b>
-          </p>
-          }
-          endMessage={
-            <p className="text-center mt-10 mb-10">
-              <b>You&apos;ve reached the end... :(</b>
-            </p>
-          }
-        >
-          <div className="flex flex-col gap-y-4">
-            {eventList.map((evento: Setlist) => {
-              const datearray = evento.eventDate.split("-");
-              var formattedDate = datearray[1] + "/" + datearray[0] + "/" + datearray[2];
-              const cardDate = new Date(formattedDate);
-              const monthNames = [
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "November",
-                "December",
-              ];
-
-              const month = monthNames[cardDate.getMonth()].slice(0, 3);
-              const day = cardDate.getDate();
-              const year = cardDate.getFullYear();
-              return (
-                <div key={evento.id} className="flex flex-col gap-y-4">
-
-                  <Link href={`/add/details?event_id=${evento.id}`}>
-                    <div
-                      className="text-base flex items-center flex-row justify-between border-b border-b-purple-500 pb-4"
-                    >
-                      <div className="flex flex-row gap-x-4 items-center">
-                        <div className="flex flex-col items-center justify-center">
-                          <div className="text-sm uppercase text-slate-500 dark:text-slate-300">{month}</div>
-                          <div className="text-lg font-semibold">{day}</div>
-                          <div className="text-sm text-slate-500 dark:text-slate-300">{year}</div>
-                        </div>
-                        <div className="flex flex-col gap-y-1">
-                          <div className="font-semibold text-base leading-normal">
-                            {evento.artist.name}
-                            {evento.tour?.name && <span>: {evento.tour?.name}</span>}
+                const month = monthNames[cardDate.getMonth()].slice(0, 3);
+                const day = cardDate.getDate();
+                const year = cardDate.getFullYear();
+                return (
+                  <div key={evento.id} className="flex flex-col gap-y-4">
+                    <Link href={`/add/details?event_id=${evento.id}`}>
+                      <div className="text-base flex items-center flex-row justify-between border-b border-b-purple-500 pb-4">
+                        <div className="flex flex-row gap-x-4 items-center">
+                          <div className="flex flex-col items-center justify-center">
+                            <div className="text-sm uppercase text-slate-500 dark:text-slate-300">
+                              {month}
+                            </div>
+                            <div className="text-lg font-semibold">{day}</div>
+                            <div className="text-sm text-slate-500 dark:text-slate-300">
+                              {year}
+                            </div>
                           </div>
-                          <div className="text-sm text-slate-500 dark:text-slate-300">
-                            {evento.venue.name}, {evento.venue.city.name}
+                          <div className="flex flex-col gap-y-1">
+                            <div className="font-semibold text-base leading-normal">
+                              {evento.artist.name}
+                              {evento.tour?.name && (
+                                <span>: {evento.tour?.name}</span>
+                              )}
+                            </div>
+                            <div className="text-sm text-slate-500 dark:text-slate-300">
+                              {evento.venue.name}, {evento.venue.city.name}
+                            </div>
                           </div>
                         </div>
+                        <Button
+                          variant="outline"
+                          className="rounded-full border-none"
+                        >
+                          <ChevronRight
+                            width={18}
+                            height={18}
+                            className="text-purple-500"
+                          />
+                        </Button>
                       </div>
-                      <Button variant="outline" className="rounded-full border-none">
-                        <ChevronRight width={18} height={18} className="text-purple-500" />
-                      </Button>
-                    </div>
-
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-        </InfiniteScroll>}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </InfiniteScroll>
+        )}
       </div>
     </div>
   );
