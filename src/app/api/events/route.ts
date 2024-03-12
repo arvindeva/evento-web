0cfc276685cc79ec98f927fddd3ce27d5a81c1bb
arvindeva <arvindeva@gmail.com>
2024-03-12 09:20:59 +0100

    bugfix: wrong query params name

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const artistMbid = searchParams.get('artistMbid')
   const year = searchParams.get('year')
-  const page = searchParams.get('page') || 1
+  const page = searchParams.get('p') || 1
   console.log(year)
 
   if (!year) {
